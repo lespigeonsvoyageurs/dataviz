22e7764fb69c8e819d8ff9b1979571ab0a8fab1c
loic.etheve <loic.etheve@lecole-ldlc.com>
2018-10-25 10:15:12 +0200

    ajout des boutons en dur (prix + mois)

--- README.md
+++
@@ -1,1 +0,0 @@
-Ceci est un README.md
--- main.js
+++ main.js
@@ -1,238 +1,97 @@
-var data = undefined;
-var margin = {top: 20, right: 20, bottom: 30, left: 40};
+var data;
+var selected_month = 1;
+var selected_price_range = "015";
 
-function legend(element, keys, z) {
-    var legendRectSize = 15;
-    var svg = d3.select('#'+element).append('svg')
-        .attr('width', 400)
-        .attr('height', 30);
 
-    var legend = svg.selectAll('.legend')
-        .data(keys)
-        .enter()
-        .append('g')
-        .attr('class', 'legend')
-        .attr('transform', function (d, i) {
-            var horz = 0 + i * 110 + 10;
-            var vert = 0;
-            return 'translate(' + horz + ',' + vert + ')';
-        });
+function draw_top_chart() {
 
-    legend.append('rect')
-        .attr('width', legendRectSize)
-        .attr('height', legendRectSize)
-        .style('fill', function (d) {
-            return z(d)
-        })
-        .style('stroke', function (d) {
-            return z(d)
-        });
+    var svg = d3.select("body").append("svg").attr("width", 800).attr("height", 200),
+        margin = {top: 20, right: 20, bottom: 30, left: 80},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom;
 
-    legend.append('text')
-        .attr('x', legendRectSize + 5)
-        .attr('y', 15)
-        .text(function (d) {
-            return d;
-        });
-}
+    var tooltip = d3.select("body").append("div").attr("class", "toolTip");
 
-function treemap(element) {
+    var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleBand().range([height, 0]);
+    var z = d3.scaleOrdinal(d3.schemeCategory10);
 
-    $("#treemap_" + element).html("");
-    $("#legend_" + element).html("");
-    var svg = d3.select("#treemap_" + element).append("svg").attr("width", 600).attr("height", 300);
-    var width = +svg.attr("width") - margin.left - margin.right;
-    var height = +svg.attr("height") - margin.top - margin.bottom;
-    var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    if (data === undefined) {
-        return;
-    }
-
-    var color = d3.scaleOrdinal(['#21DADD','#7CAD2E','#FF9F1C','#B2190E','#7C1354']);
-
-    var nested_data = d3.nest()
-        .key(function (d) {
-            return d.status;
-        })
-        .key(function (d) {
-            return d.who;
-        })
-        .rollup(function (d) {
-            return d.length;
-        })
-        .entries(data);
-
-    console.log("TREEMAP DATA");
-    console.log(nested_data);
-
-    keys = nested_data.map(function (d) {
-        return d.key;
+    var fdata = data.filter(function (d) {
+        return d.mois === selected_month;
     });
+    console.log(fdata);
+    var property = "logements_" + selected_price_range;
 
-    color.domain(keys);
-    legend("legend_" + element, keys, color);
+    x.domain([0, d3.max(fdata, function (d) {
+        return d[property];
+    })]);
+    y.domain(fdata.map(function (d) {
+        return d.ville;
+    })).padding(0.1);
 
-    var treemap = d3.treemap()
-        .size([width, height])
-        .padding(1)
-        .round(true);
+    g.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        /*.call(d3.axisBottom(x).ticks(5).tickFormat(function (d) {
+            return parseInt(d / 1000);
+        }).tickSizeInner([-height]));*/
+        .call(d3.axisBottom(x).ticks(5));
 
-    var root = d3.hierarchy({values: nested_data}, function (d) {
-        return d.values;
-    })
-        .sum(function (d) {
-            return d.value;
-        })
-        .sort(function (a, b) {
-            return b.value - a.value;
+    /*g.append("g")
+        .attr("class", "y axis")
+        .call(d3.axisLeft(y));*/
+
+    var groups = g.selectAll(".gr")
+        .data(fdata)
+        .enter().append("g")
+        .attr("class", "gr")
+        .attr("transform", function (d) {
+            return "translate(0," + y(d.ville) + ")"
         });
 
-    treemap(root);
-
-    var nodes = g.selectAll(".tm")
-        .data(root.leaves())
-        .enter().append("g")
-        .attr('transform', function (d) {
-            return 'translate(' + [d.x0, d.y0] + ')'
-        })
-        .attr("class", "tm");
-
-    nodes.append("rect")
+    groups.append("rect")
+        .attr("class", "bar")
+        .attr("x", 0)
+        .attr("height", y.bandwidth())
+        .attr("y", 0)
         .attr("width", function (d) {
-            return d.x1 - d.x0;
-        })
-        .attr("height", function (d) {
-            return d.y1 - d.y0;
+            return x(d[property]);
         })
         .attr("fill", function (d) {
-            return color(d.parent.data.key);
+            return z(d.country)
+        })
+        .on("mousemove", function (d) {
+            tooltip
+                .style("left", d3.event.pageX - 50 + "px")
+                .style("top", d3.event.pageY - 70 + "px")
+                .style("display", "inline-block")
+                .html((d[property]) + "<br>" + "logements");
+        })
+        .on("mouseout", function (d) {
+            tooltip.style("display", "none");
         });
 
-    nodes.append("text")
-        .attr("class", "tm_text")
-        .attr('dx', 4)
-        .attr('dy', 14)
+    groups.append("text")
+        .attr("dx", 30)
+        .attr("dy", 35)
         .text(function (d) {
-            return d.data.key + " " + d.data.value;
-        });
-
+            return d.ville;
+        })
 }
 
-function bar_chart(element, property) {
-    $("#" + element).html("");
-    var svg = d3.select("#" + element).append("svg").attr("width", 300).attr("height", 300);
-    var width = +svg.attr("width") - margin.left - margin.right;
-    var height = +svg.attr("height") - margin.top - margin.bottom;
-    var g = svg.append("g")
-        .attr("transform","rotate(90)", "translate(" + margin.left + "," + margin.top + ")");
+d3.json("data.json", function (error, d) {
+    if (error) throw error;
 
-    var nested_data = d3.nest()
-        .key(function (d) {
-            return d[property];
-        })
-        .rollup(function (d) {
-            return {
-                size: d.length, total_time: d3.sum(d, function (d) {
-                    return d.time;
-                })
-            };
-        })
-        .entries(data);
-
-    nested_data = nested_data.sort(function (a, b) {
-        return d3.ascending(a.key, b.key)
+    data = d;
+    data.sort(function (a, b) {
+        return a.logements - b.logements;
     });
 
-
-    console.log("BARCHART DATA");
-    console.log(nested_data);
-
-    if (property ==="time"){
-        var x = d3.scaleLinear()
-            .rangeRound([0, width]);}
-
-    else{
-        x = d3.scaleBand()
-            .rangeRound([0, width])
-            .paddingInner(0.1);
-    }
-
-    var y = d3.scaleLinear()
-        .rangeRound([height, 0]);
-
-    var z = d3.scaleOrdinal(['#7C1354','#B2190E','#FF9F1C','#7CAD2E','#21DADD']);
-
-    if (property === "time") {
-        x.domain([0, d3.max(nested_data.map(function (d) {
-            return +d.key;
-        })) + 1]);
-    } else {
-        x.domain(nested_data.map(function (d) {
-            return d.key;
-        }));
-
-    }
-
-    y.domain([0, d3.max(nested_data, function (d) {
-        return d.value.size;
-    })]);
-    z.domain(nested_data.map(function (d) {
-        return d.key;
-    }));
-
-    g.selectAll(".bar")
-        .data(nested_data)
-        .enter()
-        .append("rect")
-        .attr("class", "bar")
-        .attr("x", function (d) {
-            return x(d.key)
-        })
-        .attr("y", function (d) {
-            return y(d.value.size)
-        })
-        .attr("height", function (d) {
-            return height - y(d.value.size);
-        })
-        .attr("width", function (d) {
-            if (property ==="time"){
-                return(10);
-            }
-            else{
-                return x.bandwidth();
-            }
-            return (1000);
-        })
-        .style("fill", function (d) {
-            return z(d.key)
-
-        });
-
-    g.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    g.append("g")
-        .attr("class", "axis")
-        .call(d3.axisLeft(y).ticks(null, "s"))
-}
-
-$(function () {
-    console.log("READY");
-
-    var URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQfeT9lPtJ5ia2XsopWVdvl98Oy7Bu6xL9SVQBEh32OXC8Qk4MKYxr2TcGSSTkAs7kAMfjF83IEGhQ-";
-    URL += "/pub?single=true&output=csv";
-
-
-    d3.csv(URL, function (d) {
-        data = d;
-        data.forEach(function (d) {
-            d.time = +d.time;
-        });
-        bar_chart("bcs", "status");
-
-    });
+    console.log(data);
+    draw_top_chart();
 
 });
+
